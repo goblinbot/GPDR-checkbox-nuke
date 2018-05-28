b4d259d0ab9d86afdae0834a7d6574f50febb2dc
GGBOT <th.boerma@gmail.com>
2018-05-28 21:13:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var checkboxes = new Array(); 
+  checkboxes = document.getElementsByTagName('input');
+ 
+  for (var i=0; i<checkboxes.length; i++)  {
+    if (checkboxes[i].type == 'checkbox')   {
+      checkboxes[i].checked = false;
+    }
+  }
